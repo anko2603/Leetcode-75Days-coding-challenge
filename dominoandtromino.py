c52bc45d2e438ad5f316f1b2ac8538b8a05ceda7
Ankita <gautamankita2603@gmail.com>
2025-01-22 23:50:48 +0530

    Domino and Tromino Tiling

--- dominoandtromino.py
+++ dominoandtromino.py
@@ -6,14 +6,22 @@
 # Output: 5
 class Solution:
     def numTilings(self, n: int) -> int:
-        MOD = 10**9 + 7
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
-        dp0, dp1, dp2 = 1, 1, 2  
-        for i in range(3, n + 1):
-            current = (dp2 + dp1 + dp0 * 2) % MOD
-            dp0, dp1, dp2 = dp1, dp2, current
-        return dp2
+        MOD = 1e9 + 7
+        if n < 3:
+            return n
+        dp = [[0] * 3 for _ in range(n + 1)]
+        dp[0][0] = dp[1][0] = 1
+        dp[1][1] = dp[1][2] = 1
+        for i in range(2, n + 1):
+            dp[i][0] = (dp[i - 1][0] +
+                        dp[i - 2][0] +
+                        dp[i - 2][1] +
+                        dp[i - 2][2]) % MOD
+            dp[i][1] = (dp[i - 1][0] +
+                        dp[i - 1][2]) % MOD
+            dp[i][2] = (dp[i - 1][0] +
+                        dp[i - 1][1]) % MOD
+        return int(dp[n][0])
+
+
 
